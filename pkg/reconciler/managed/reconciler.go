7f7933bcfde723fec9cc35f30bec7141d591ca5e
elvis <elvis.cai@airwallex.com>
2023-05-17 20:10:54 +1000

    test

--- pkg/reconciler/managed/reconciler.go
+++ pkg/reconciler/managed/reconciler.go
@@ -18,7 +18,6 @@ package managed
 
 import (
 	"context"
-	"fmt"
 	"strings"
 	"time"
 
@@ -873,8 +872,7 @@ func (r *Reconciler) Reconcile(ctx context.Context, req reconcile.Request) (reco
 		}
 		managed.SetConditions(xpv1.ReconcileSuccess())
 		managed.SetConditions(xpv1.Available())
-		fmt.Println("nihao")
-		managed.SetAnnotations(map[string]string{"nihao": "nihao"})
+		managed.SetLabels(map[string]string{"mylabel": "myvalue"})
 		// if the merge request annotation is removed, we will have a chance to reconcile again and resume
 		// and if status update fails, we will reconcile again to retry to update the status
 		return reconcile.Result{}, errors.Wrap(r.client.Status().Update(ctx, managed), errUpdateManagedStatus)
